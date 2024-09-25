83c250c414650d9d623c47a3462f798539899038
ThuanVan Nguyen <nguyenthuankma@gmail.com>
2024-09-25 23:25:59 +0700

    Update App.vue

--- 07/src/App.vue
+++ 07/src/App.vue
@@ -2,10 +2,18 @@
 import { ref } from "vue";
 
 const count = ref(0);
+const greet = (event) => {
+  console.log(event);
+  alert(`Xin chao ${event.target.value}`);
+};
 </script>
 
 <template>
   <div>
     <h1>{{ count }}</h1>
+    <button @click="count++">Thêm 1</button>
+    <br />
+    <br />
+    <button @click="greet">Chào</button>
   </div>
 </template>

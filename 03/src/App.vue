e680c9b34f85e875c3c0c090049943bec38067fb
thuan.nv <nguyenthuankma@gmail.com>
2024-09-25 13:48:39 +0700

    update b03

---
+++ 03/src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { computed, ref } from "vue";
+
+const firstName = ref("Nguyen Van");
+const lastName = ref("A");
+
+const fullName = computed({
+  get() {
+    return firstName.value + " " + lastName.value;
+  },
+  set(newValue) {
+    [firstName.value, lastName.value] = newValue.split(",");
+  },
+});
+
+const handleChangeName = () => {
+  fullName.value = "Nguyen van, teo"
+};
+</script>
+
+<template>
+  <p>FullName: {{ fullName }}</p>
+  <button @click="handleChangeName">Change Name</button>
+</template>

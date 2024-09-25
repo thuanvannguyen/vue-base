d9b65f5cb69bb8394fe2f7ddf45bc5d53429113d
thuan.nv <nguyenthuankma@gmail.com>
2024-09-25 10:22:45 +0700

    update b2

--- 02/src/App.vue
+++ 02/src/App.vue
@@ -1,7 +1,16 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+
+const count = ref('hello 500 anh em!')
+console.log(count.value)
+
+// Tuong tac qua phan value
+</script>
 
 <template>
-  <h1>Hello 500 anh em</h1>
+  <h1>{{ count }}</h1>
+
+  <!-- Doi voi template khong can value -->
 </template>
 
 <style scoped></style>

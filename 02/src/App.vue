2423b92cced96e5608dd6d54727d3d0763e0e12c
thuan.nv <nguyenthuankma@gmail.com>
2024-09-25 10:51:20 +0700

    Update App.vue

--- 02/src/App.vue
+++ 02/src/App.vue
@@ -1,17 +1,39 @@
 <script setup>
-import { ref } from 'vue'
+import { nextTick, ref } from 'vue'
 
 const count = ref(3)
 console.log(count.value)
 
+const object = ref({
+  user: 'test',
+  age: '30'
+})
+
 // script: Tuong tac qua phan value
+
+// 2. nextTick()
+const number = ref(0)
+
+const handleIncrement = async () => {
+  number.value++
+  console.log(document.getElementById('count').innerText)
+  await nextTick()
+  console.log(document.getElementById('count').innerText)
+}
 </script>
 
 <template>
   <h1>{{ count }}</h1>
   <button @click="count++">Increment</button>
 
-  <!-- Template khong can value -->
+  <h1>{{ object.user }}</h1>
+
+  <!-- // 2. nextTick() -->
+  <h3 id="count">{{ number }}</h3>
+
+  <button @click="handleIncrement">Incree</button>
 </template>
 
 <style scoped></style>
+
+<!-- Template khong can value -->

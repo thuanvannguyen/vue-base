7409165fd5af56a54746cd8034340ef11e0122b8
ThuanVan Nguyen <nguyenthuankma@gmail.com>
2024-09-25 23:44:04 +0700

    update b8

---
+++ 08/src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+const sayHi1 = () => {
+  alert(`Xin chào 1`);
+};
+const sayHi2 = () => {
+  alert(`Xin chào 2`);
+};
+const submit = () => {
+  alert("Form submitted");
+};
+</script>
+
+<template>
+  <h1>Event Handling</h1>
+  <button @click="sayHi2">
+    <button @click.stop="sayHi1">Chào 1</button>
+    say hi 2
+  </button>
+
+  <form @submit.prevent="submit">
+    <button type="submit">Submit</button>
+  </form>
+</template>
+
+
+<!-- .stop ngăn chặn sự kiện pt con lên pt -->
+<!-- .prevent ngăn chặn sự kiện load lai cua trinh duyet -->

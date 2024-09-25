9b72092b13e8b85652f3ff79eb66bb647e940421
thuan.nv <nguyenthuankma@gmail.com>
2024-09-25 14:14:37 +0700

    Update App.vue

--- 04/src/App.vue
+++ 04/src/App.vue
@@ -1,7 +1,39 @@
 <script setup>
+import { ref } from "vue";
+
+// binding qua doi tuong
+const isAcitve = ref(false);
 </script>
 
-<template></template>
+<template>
+  <div>
+    <button class="button" :class="{ active: isAcitve }">Active</button>
+  </div>
+</template>
 
 <style scoped>
+button {
+  border: none;
+  background: gray;
+  color: #fff;
+  padding: 5px 15px;
+  border-radius: 3px;
+  cursor: pointer;
+}
+.active {
+  border: none;
+  background: green;
+  color: #fff;
+  padding: 5px 15px;
+  border-radius: 3px;
+  cursor: pointer;
+}
+.error {
+  border: none;
+  background: red;
+  color: #fff;
+  padding: 5px 15px;
+  border-radius: 3px;
+  cursor: pointer;
+}
 </style>
